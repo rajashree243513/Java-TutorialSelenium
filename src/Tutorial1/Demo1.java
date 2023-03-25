4c93d485ea35ffe0da72f891b7eb7e8bde5bedc5
admin <admin@DESKTOP-79D13NM>
2023-03-25 15:29:23 +0530

    Rajashree branch created

--- src/Tutorial1/Demo1.java
+++ src/Tutorial1/Demo1.java
@@ -1,14 +1,26 @@
 package Tutorial1;
 
+import java.util.ArrayList;
+
 public class Demo1 {
 
 	public static void main(String[] args) {
 
-int a=100,b=300,c=366;
-System.out.println(a);
-System.out.println(b)
-;
-System.out.println(c);
+		int abc[] = { 100, 200, 400, 200 };
+		//3<4
+for (int i = 0; i < abc.length; i++) 
+{				
+			//4<4
+for (int j = i + 1; j < abc.length; j++) 
+{
+	System.out.println(abc[j]);
+		//200  400 200
+		//400  200
+		//200
+
 	}
 
+
+}
+	}
 }
